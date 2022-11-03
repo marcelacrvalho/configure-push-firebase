12a068e31e840d97e20a78f3fb688b4f2a5803c5
Marcela <marcelacrv@icloud.com>
2022-11-03 14:59:01 -0300

    Update push_firebase.dart

--- push_firebase.dart
+++ push_firebase.dart
@@ -1,7 +1,3 @@
-// 1. importar os packages flutter local notifications, firebase messaging e firebase core
-// 2. Cria uma classe para poder conversar com as notificações que serão recebidas do firebase, de forma remota
-
-
 class PushNotificationService {
 
     final CustomLocalNotification _customLocalNotification;
@@ -9,15 +5,11 @@ class PushNotificationService {
     PushNotificationService._internal(this._customLocalNotification);
     
     static final PushNotificationService _singleton = PushNotificationService._internal(CustomLocalNotification());
-    factory PushNotificationService() => _singleton; /* TODA VEZ QUE EU CHAMAR A MINHA FACTORY, ELA RECUPERA A INSTANCIA DO SIGLETON 
-                                                     OU SEJA, EU TEREI UMA ÚNICA INSTANCIA DE PUSHNOTIFICATIONSERVICE ATÉ O FIM DA MINHA APLICAÇÃO */
-   
+    factory PushNotificationService() => _singleton;
 
     Future<void> initialize() async {
-    // COMO MINHA APLICAÇÃO APARECERÁ PARA O USUÁRIO, com BADGE, SOM, IMAGEM, etc  
     await FirebaseMessaging.instance.setForegroundNotificationPresentationsOptions(badge: true, sound: true);
     
-    // QUANDO O USER RECEBER A MENSAGEM, O QUE O APP FARÁ
     FirebaseMessaging.onMessage.listen((message) {  // RECEBE UMA STREAM DE REMOTE MESSAGEM
         RemoteNotification? notification = message.notification;
         AndroidNotification? android = message.notification?.android;
@@ -27,13 +19,11 @@ class PushNotificationService {
     }
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      // FAZ ALGUMA COISA AQUI QUANDO O USUÁRIO ABRE A MENSAGEM. ENVIA PARA UMA ROTA OU
-        VERIFICA SE (message.data['chave-escolhida'] == chave) PRA PODER ENVIAR PRA UMA ROTA
+        //TODO: chamar a route espec
     });
     
     getTokenFirebase() {
-        await FirebaseMessage.instance.getToken(); /* GUARDA ESSE TOKEN EM UM BD PARA PODER
-                                                    CONVERSAR COM O USER QUANDO FOR NECESSÁRIO */
+        await FirebaseMessage.instance.getToken();
     }
   }
 }
