c53a200074da839ae44200fa569f1974078ba785
Marcela <marcelacrv@icloud.com>
2022-11-03 10:57:22 -0300

    Update local_notification.dart

--- local_notification.dart
+++ local_notification.dart
@@ -1,45 +1,65 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 class CustomLocalNotification {
-  late FlutterLocalNotificationsPlugin localNotifications;
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
   late AndroidNotificationChannel channel;
-  
+
   CustomLocalNotification() {
-   channel = const AndroidNotificationChannel(
-     'high_importance_channel',
-     'High Importance Notifications'
-     description: 'Channel for Android Notifications',
-     importance: Importance.max,
-   );
-    
-    _configureAndroid().then(
+    channel = const AndroidNotificationChannel(
+      'high_importance_channel',
+      'High Importance Notifications',
+      description: 'This channel is used for important notifications.',
+      importance: Importance.max,
+    );
+
+    _configuraAndroid().then(
       (value) {
-        localNotifications = value;
-        initializeNofitications();
-      }
-    )
-  }
-  
-  initializeNotifications() {
-    const AndroidInitializationSettings androidSettings =
-        AndroidInitializationSettings('@drawable/ic_launcher');
-    
-    const DarwinInitializationSettings iOSSettings =
-        DarwinInitializationSettings(
-      requestSoundPermission: false,
-      requestBadgePermission: false,
-      requestAlertPermission: false,
+        flutterLocalNotificationsPlugin = value;
+        inicializeNotifications();
+      },
     );
-    
-    await localNotifications.initialize(
-      const InitializeSettings(
-        android: androidSettings,
-        iOS: iOSSettings,
+  }
+
+  Future<FlutterLocalNotificationsPlugin> _configuraAndroid() async {
+    var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    return flutterLocalNotificationsPlugin;
+  }
+
+  inicializeNotifications() {
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final iOS = IOSInitializationSettings(
+      onDidReceiveLocalNotification: (_, __, ___, ____) {},
+    );
+
+    flutterLocalNotificationsPlugin.initialize(
+      InitializationSettings(android: android, iOS: iOS),
+    );
+  }
+
+  androidNotification(
+    RemoteNotification notification,
+    AndroidNotification android,
+  ) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          icon: android.smallIcon,
+        ),
       ),
-      onSelectNotification: _onSelectedNotification,
     );
   }
-  
-  _onSelectedNotification() {
-    if(payLoad){}
-  }
-  
 }
